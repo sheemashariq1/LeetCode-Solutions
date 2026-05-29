daa16ac5b4589f23acb037c048b26acaa656d8f3
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-05-29 14:48:42 +0530

    Time: 1862 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -5,9 +5,9 @@ SELECT d.name AS Department,
 FROM Employee e
 JOIN Department d
 ON e.departmentId = d.id
-WHERE 3 > (
-    SELECT COUNT(DISTINCT e2.salary)
-    FROM Employee e2
-    WHERE e2.departmentId = e.departmentId
-      AND e2.salary > e.salary
-);
+WHERE (
+    SELECT COUNT(DISTINCT salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+      AND salary > e.salary
+) < 3;
