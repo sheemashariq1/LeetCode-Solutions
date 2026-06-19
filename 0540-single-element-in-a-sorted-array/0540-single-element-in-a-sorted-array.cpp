1b9e13c6ff6eb4cc70e586e2628b710f5d6c8854
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-06-19 11:07:46 +0530

    Time: 0 ms (100%), Space: 26.3 MB (33.86%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int res = 0;
+        for (int num : nums)
+        {
+            res ^= num;
+        }
+        return res;
+    }
+};
