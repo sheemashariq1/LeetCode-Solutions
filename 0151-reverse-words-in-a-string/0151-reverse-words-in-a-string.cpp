3af60a271e9d9c1377a1638b8fc347243d578a23
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-06-24 11:16:03 +0530

    Time: 0 ms (100%), Space: 9.9 MB (88.18%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        std::string result = "";
+        string word = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                word = s[i] + word;
+            }
+            else if (!word.empty()) {
+                if (!result.empty()) result += " ";
+                result += word;
+                word = "";
+            }
+        }
+            if (!word.empty()) {
+            if (!result.empty()) result += " ";
+            result += word;
+        }
+        return result;
+    }
+};
