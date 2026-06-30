6e841a95a69aa5dfffeacee6d8d410d5282b6c4c
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-06-30 12:59:58 +0530

    Time: 0 ms (100%), Space: 9.9 MB (55.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+bool check_string(string& s,int l,int r){
+    if(l>=r){
+        return true;
+    }
+    else if(!isalnum(s[l])){
+     return check_string(s,l+1,r);
+    }
+    else if(!isalnum(s[r])){
+        return check_string(s,l,r-1);
+    }
+    else if(tolower(s[l])!=tolower(s[r])){
+        return false;
+    }
+    return check_string(s,l+1,r-1);
+}
     bool isPalindrome(string s) {
-       string clr = "";
-        for (char ch : s) {
-            if (isalnum(ch)) {
-                clr += tolower(ch);
-            }
-        }
-        string rev = clr;
-        reverse(rev.begin(), rev.end());
-        return clr == rev;
+        return check_string(s,0,s.length()-1);
     }
 };
