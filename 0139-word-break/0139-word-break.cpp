0c803226274b2bc70016ac2bea6cc7b6d7aeba14
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-07-04 17:01:09 +0530

    Time: 16 ms (19.52%), Space: 16 MB (45.6%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> result(wordDict.begin(), wordDict.end());
-        unordered_map<int, bool> memo; 
-        return solve(s, 0, result, memo);
-    }
-    bool solve(string& s, int start, unordered_set<string>& result,
-               unordered_map<int, bool>& memo) {
-        if (start == s.size()) return true;
-        if (memo.count(start)) return memo[start];
-        for (int end = start + 1; end <= s.size(); end++) {
-            string word = s.substr(start, end - start);
-            if (result.count(word) && solve(s, end, result, memo)) {
-                return memo[start] = true;
+    int n = s.length();
+		unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+		vector<bool> dp(n + 1, false);
+		dp[0] = true;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if(dp[j]&&wordSet.count(s.substr(j,i-j)))
+                {
+                    dp[i] = true;
+                    break;
+				}
             }
         }
-        return memo[start] = false;
+        return dp[n];
     }
 };
