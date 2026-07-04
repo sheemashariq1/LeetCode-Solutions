61f3b5ca13efa920e057dca18516213a0134c91c
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-07-04 17:02:29 +0530

    Time: 0 ms (100%), Space: 10.7 MB (84.67%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -11,7 +11,7 @@ unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
         dp[0] = true; 
         
         for (int i = 1; i <= n; ++i) {
-            for (int j = i - 1; j >= max(0, i - maxLength); --j) {
+            for (int j = i - 1; j >= max(0, i - maxLength); j--) {
                 if (dp[j] && wordSet.count(s.substr(j, i - j))) {
                     dp[i] = true;
                     break; 
