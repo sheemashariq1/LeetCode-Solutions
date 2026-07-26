7dcbb431995df861d2e026e2c2601ab9ae15cd8c
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-07-26 19:03:45 +0530

    Time: 3 ms (37.71%), Space: 9 MB (8.37%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,12 @@
+bool isPalindrome(int x) {
+        if (x < 0) {
+        return false;
+    }
+    int org = x;
+    long long rev = 0; 
+    while (x > 0) {
+        rev = (rev * 10) + (x % 10);
+        x /= 10;
+    }
+    return rev == org;
+}
