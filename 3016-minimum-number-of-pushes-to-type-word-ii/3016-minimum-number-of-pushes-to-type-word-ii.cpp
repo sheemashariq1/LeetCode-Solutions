a799e247876b5a0c3c570da83f1bf3a15b44437e
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-07-31 15:14:25 +0530

    Time: 4 ms (86.59%), Space: 25.5 MB (61.34%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-         vector<int> freq(26, 0);
-        for (char ch : word) {
+          vector<int> freq(26, 0);
+        for (char ch : word)
             freq[ch - 'a']++;
-        }
         sort(freq.begin(), freq.end(), greater<int>());
         int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] == 0) break;
-
-            int pushes = i / 8 + 1;
-            ans += freq[i] * pushes;
-        }
-       return ans;
+        for (int i = 0; i < 26; i++)
+            ans += freq[i] * (i / 8 + 1);
+        return ans;
     }
 };
