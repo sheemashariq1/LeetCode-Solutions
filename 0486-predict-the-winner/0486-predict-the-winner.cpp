38f9a9eae422cac1dbc74b52e4df437285faa034
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-08-01 13:22:15 +0530

    Time: 1 ms (46.17%), Space: 9.6 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        if (n <= 1) return true;
-        vector<int> result(nums);
+        vector<int> result(nums); 
         for (int i = n - 2; i >= 0; i--) {
             for (int j = i + 1; j < n; j++) {
                 result[j] = max(nums[i] - result[j], nums[j] - result[j - 1]);
