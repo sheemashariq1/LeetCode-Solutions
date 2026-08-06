12ba823f6848ebbbd5e0e0a5f87acf94c471490c
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-08-06 13:10:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.77%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+         for (int i = n; ; i++) {
+            int num = i;
+            int prod = 1;
+            while (num > 0) {
+                prod *= num % 10;
+                num /= 10;
+            }
+            if (prod % t == 0)
+                return i;
+        }
+    }
+};
