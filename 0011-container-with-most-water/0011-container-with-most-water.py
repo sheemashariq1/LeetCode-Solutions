0ccc9678fa21210eea84f0c5544a744949d1a826
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-08-09 15:08:20 +0530

    Time: 114 ms (72.51%), Space: 20.7 MB (24.65%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -4,18 +4,14 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        left = 0
-        right = len(height) - 1
+        l = 0
+        r = len(height) - 1
         res = 0
-
-        while left < right:
-            width = right - left
-            h = min(height[left], height[right])
-            area = width * h
-            res = max(res, area)
-            if height[left] < height[right]:
-                left += 1
+        while l < r:
+            res = max(res, (r - l) * min(height[l], height[r]))
+            if height[l] < height[r]:
+                l += 1
             else:
-                right -= 1
+                r -= 1
         return res
         
