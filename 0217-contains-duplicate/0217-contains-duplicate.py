933f192b8be684b67e28408f6ae99333324f2d0e
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-08-12 21:14:15 +0530

    Time: 27 ms (50.83%), Space: 25.9 MB (67.97%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,13 +1,11 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
         dupp = set()
+
         for num in nums:
             if num in dupp:
                 return True
             dupp.add(num)
+            
         return False
         
