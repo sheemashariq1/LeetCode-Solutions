01f50eca73dc77966efa821b3441e97f52da17a4
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-08-21 20:31:47 +0530

    Time: 0 ms (100%), Space: 24 MB (19.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-               int j = 0;
+        int j = 0;
 
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
