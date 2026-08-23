7046e5e264b1a6302718712a019b0360287299ea
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-08-23 12:21:47 +0530

    Time: 5 ms (30.77%), Space: 13.9 MB (92.07%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int sum1 = 0, sum2 = 0;
+        int q1 = 0, q2 = 0;
+
+        for(int i = 0; i < n/2; i++) {
+            if(num[i] == '?') q1++;
+            else sum1 += num[i] - '0';
+        }
+
+        for(int i = n/2; i < n; i++) {
+            if(num[i] == '?') q2++;
+            else sum2 += num[i] - '0';
+        }
+
+        if((q1 + q2) % 2)
+            return true;
+
+        return 2 * (sum1 - sum2) != 9 * (q2 - q1);
+    }
+};
