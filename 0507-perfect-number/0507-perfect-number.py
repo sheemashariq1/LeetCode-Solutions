e8118391b1bbfab576a49d1ebe5c077abaf2ca1b
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-08-26 17:44:55 +0530

    Time: 3 ms (92.31%), Space: 12.6 MB (38.02%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,16 +1,11 @@
 class Solution(object):
     def checkPerfectNumber(self, num):
-        """
-        :type num: int
-        :rtype: bool
-        """
-        if num <= 1:
+        if num<=1:
             return False
-
-        div = 1 
-        for i in range(2, num//2+1):
-            if num % i == 0:
-                div += i
-
-        return div == num
-        
+        div=1
+        for i in range(2,int(math.sqrt(num)+1)):
+            if num%i==0:
+                div+=i
+                if i!=num//i:
+                    div+=num//i
+        return div==num
