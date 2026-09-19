9a3b6cf1c6f186c4bc1205d882a4fa6f1662060b
SHEEMA SHARIQ <sheemashariq10@gmail.com>
2026-09-19 11:27:29 +0530

    Time: 89 ms (26.15%), Space: 12.7 MB (54.29%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
+        res = []
+        for h in sorted(heights, reverse=True):
+            i = heights.index(h)
+            res.append(names[i])
+        return res
